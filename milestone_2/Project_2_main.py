84c1ee462ed9833f177735e9a5710ad7ae520933
JanikRohrer <71765633+JanikRohrer@users.noreply.github.com>
2020-10-29 22:08:31 +0100

    Add files via upload
    I modularized the code and have uploaded the main and the modules.

---
+++ milestone_2/Project_2_main.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.datasets import imdb
+from keras.models import load_model
+import numpy as np
+import buildModel
+import save_load
+
+model=buildModel.BuildModel()
+save_load.saveModel(model,"Model.h5")
+
+
+# Predict sentiment from reviews
+
+# reverse lookup
+word_to_id = imdb.get_word_index()
+'''word_to_id = {k: (v + INDEX_FROM) for k, v in word_to_id.items()}'''
+word_to_id["<PAD>"] = 0
+word_to_id["<START>"] = 1
+word_to_id["<UNK>"] = 2
+
+bad = "this movie was terrible and bad"
+good = "i really liked the movie and had fun"
+for review in [good, bad]:
+    tmp = []
+    for word in review.split(" "):
+        tmp.append(word_to_id[word])
+    tmp_padded = sequence.pad_sequences([tmp], maxlen=400)
+    print("%s. Sentiment: %s" % (
+        review, model.predict(np.array([tmp_padded[0]]))))
