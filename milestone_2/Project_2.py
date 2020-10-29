a316dafc634bc9a4f28dada85daa8e632bb577b0
Aristidis Kotatis <a.kotatis7@hotmail.com>
2020-10-29 18:45:39 +0100

    trying to functinize the code

--- milestone_2/Project_2.py
+++ milestone_2/Project_2.py
@@ -9,95 +9,86 @@ from keras.datasets import imdb
 from keras.models import load_model
 import numpy as np
 
-# set parameters:
-max_features = 5000
-maxlen = 400
-batch_size = 32
-embedding_dims = 50
-filters = 250
-kernel_size = 3
-hidden_dims = 250
-epochs = 2
-
-# Load Data with Keras
-
-print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+def getParameters(max_features,maxlen,batch_size,embedding_dims,filters,kernel_size,hidden_dims,epochs):
+	max_features = 5000
+	maxlen = 400
+	batch_size = 32
+	embedding_dims = 50
+	filters = 250
+	kernel_size = 3
+	hidden_dims = 250
+	epochs = 2
+	return features,maxlen,batch_size,embedding_dims,filters,kernel_size,hidden_dims,epochs
 
 
-# Build a fitted model
+def loadData(x_train,y_train,x_test,y_test):
+	print('Loading data...')
+	(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+	print(len(x_train), 'train sequences')
+	print(len(x_test), 'test sequences')
+	print('Pad sequences (samples x time)')
+	x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+	x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+	print('x_train shape:', x_train.shape)
+	print('x_test shape:', x_test.shape)
+	return x_train,y_train,x_test,y_test
 
-print('Build model...')
-model = Sequential()
 
-# we start off with an efficient embedding layer which maps
-# our vocab indices into embedding_dims dimensions
-model.add(Embedding(max_features,
+def buildModel(model):
+	print('Build model...')
+	model = Sequential()
+	model.add(Embedding(max_features,
                     embedding_dims,
                     input_length=maxlen))
-model.add(Dropout(0.2))
-
-# we add a Convolution1D, which will learn filters
-# word group filters of size filter_length:
-model.add(Conv1D(filters,
+	model.add(Dropout(0.2))
+	model.add(Conv1D(filters,
                  kernel_size,
                  padding='valid',
                  activation='relu',
                  strides=1))
-# we use max pooling:
-model.add(GlobalMaxPooling1D())
+	model.add(GlobalMaxPooling1D())
+	model.add(Dense(hidden_dims))
+	model.add(Dropout(0.2))
+	model.add(Activation('relu'))
+	model.add(Dense(1))
+	model.add(Activation('sigmoid'))
+	return model
 
-# We add a vanilla hidden layer:
-model.add(Dense(hidden_dims))
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-
-# We project onto a single unit output layer, and squash it with a sigmoid:
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
+def compileModel(model,fit):
+	model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-fit=model.fit(x_train, y_train,
+	fit=model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           validation_data=(x_test, y_test))
+	return model,fit
 
 
-# Save fitted model as .h5-file
-model.save("my_h5_model.h5")
+def saveModel(model):
+	return model.save("my_h5_model.h5")
 
 
-# Load the model as .h5-file
-model = load_model('my_h5_model.h5')
+def loadModel(model):
+	model = load_model('my_h5_model.h5')
+	return model
 
-# Predict sentiment from reviews
+def getReview(word_to_id,bad,good, tmp_padded,tmp):
+	word_to_id = imdb.get_word_index()
+	word_to_id["<PAD>"] = 0
+	word_to_id["<START>"] = 1
+	word_to_id["<UNK>"] = 2
 
-# reverse lookup
-word_to_id = imdb.get_word_index()
-'''word_to_id = {k: (v + INDEX_FROM) for k, v in word_to_id.items()}'''
-word_to_id["<PAD>"] = 0
-word_to_id["<START>"] = 1
-word_to_id["<UNK>"] = 2
-
-bad = "this movie was terrible and bad"
-good = "i really liked the movie and had fun"
-for review in [good, bad]:
-    tmp = []
-    for word in review.split(" "):
-        tmp.append(word_to_id[word])
-    tmp_padded = sequence.pad_sequences([tmp], maxlen=maxlen)
-    print("%s. Sentiment: %s" % (
-        review, model.predict(np.array([tmp_padded[0]]))))
+	bad = "this movie was terrible and bad"
+	good = "i really liked the movie and had fun"
+	for review in [good, bad]:
+ 	   tmp = []
+ 	   for word in review.split(" "):
+ 	       tmp.append(word_to_id[word])
+ 	   tmp_padded = sequence.pad_sequences([tmp], maxlen=maxlen)
+ 	   print("%s. Sentiment: %s" % (
+ 	       review, model.predict(np.array([tmp_padded[0]]))))
+	return tmp_padded
 
 
 """import matplotlib.pyplot as plt
