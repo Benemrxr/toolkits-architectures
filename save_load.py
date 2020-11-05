28e54a8ca905f9d7d90cc4b8bd4283e47731db47
JanikRohrer <71765633+JanikRohrer@users.noreply.github.com>
2020-11-05 21:04:01 +0100

    Code after implemented the changes from the feedback.
    Can be used for project 3

---
+++ save_load.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.models import load_model
+
+# Save fitted model as .h5-file
+def save_model(model,filename):
+	model.save(str(filename))
+
+
+# Load the model as .h5-file
+def load_model(filename):
+	model = load_model(str(filename))
+	return model
+
